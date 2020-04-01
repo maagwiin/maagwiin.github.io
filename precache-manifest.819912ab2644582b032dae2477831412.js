4c9de510bad6cf69ff80c5fd890d4ffd7043cdce
maagwiin <magnuwindell@gmail.com>
2020-04-01 12:11:40 -0300

    Updates

---
+++ precache-manifest.819912ab2644582b032dae2477831412.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def369f18382d22505c9679121de7f81",
+    "url": "/maagwiin.github.io/index.html"
+  },
+  {
+    "revision": "43169d24a2866568abd8",
+    "url": "/maagwiin.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "451367a49253728bfacb",
+    "url": "/maagwiin.github.io/static/js/2.4c2ea9d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maagwiin.github.io/static/js/2.4c2ea9d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43169d24a2866568abd8",
+    "url": "/maagwiin.github.io/static/js/main.a5711512.chunk.js"
+  },
+  {
+    "revision": "3cf3f9888684f669f272",
+    "url": "/maagwiin.github.io/static/js/runtime-main.276a8b77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/maagwiin.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
